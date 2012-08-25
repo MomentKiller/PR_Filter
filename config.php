127571ad4f1f361934ec7375a21ece18d79d3278
germanidiot <99b22d21e002e14a88c8a77813893c25>
2012-08-25 22:55:13 +0200

    add config file

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * MySQL Daten
+ */
+
+	$dbhost = '';
+	$dbuser = '';
+	$dbpass = '';
+	$dbname = '';
+
+/*
+ * Allgemeine Einstellungen
+ */
+
+	$charset = 'utf-8';
+	$googleanalytics = '';
+
+/*
+ * Error Reporting
+ */
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+/*
+ * MySQL Verbindung
+ */
+
+	$db = @new MySQLi($dbhost, $dbuser, $dbpass, $dbname);
+	if (mysqli_connect_errno()) {
+		die('Konnte keine Verbindung zu Datenbank aufbauen, MySQL meldete: '.mysqli_connect_error());
+	}
+?>
